2ccf09793447e3ab4f899b3439e9b5e4e66037e2
VISHAL SHARMA <57282548+nick-arch@users.noreply.github.com>
2024-06-03 22:23:10 +0530

    Update रूपांतरक_Clone_Repo.py

--- रूपांतरक_Clone_Repo.py
+++ रूपांतरक_Clone_Repo.py
@@ -8,9 +8,10 @@ import os
 from IPython.display import display, HTML
 import ipywidgets as widgets
 
-# CSS for styling
+# नमस्ते रुद्रमन्यव उतोत इषवेनमः |
 gradient_button_css = """
 <style>
+
 .progress {
     width: 100%;
     height: 30px;
@@ -44,8 +45,7 @@ gradient_button_css = """
 }
 </style>
 """
-
-# Displaying CSS
+# नमः प्रत्नय तॊषया गिरिश विप्रेषया ॥१॥
 display(HTML(gradient_button_css))
 
 # Function to generate the main repo URL
@@ -57,7 +57,7 @@ def generate_main_repo_url():
 # Function to display the gradient text logo
 def display_logo():
     logo_html = """
-    <div style='text-align: center;'><h1 style='font-family: Andika, sans-serif; font-size: 50px; background: -webkit-linear-gradient(left, #2196f3, #800080); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'><span style='color: #ff0066;'>र</span><span style='color: #ff6f00;'>ू</span><span style='color: #ffjd00;'>प</span><span style='color: #4caf50;'>ा</span><span style='color: #2196f3;'>ं</span><span style='color: #9c27b0;'>त</span><span style='color: #ff5722;'>र</span><span style='color: #FFC0CB;'>क</span> <span style='color: #2196f3; font-size: 22px;'>~ Rupantarak</span><br><span style='font-size: 18px;'>By Vishal Sharma</span><br><span style='color: #ff0066; font-size: 22px;'>ॐ नमः पार्वती पतये, हर-हर महादेव:</span></h1></div>
+    <div style='text-align: center;'><h1 style='font-family: Andika, sans-serif; font-size: 50px; background: -webkit-linear-gradient(left, #FA8072, #F5F5DC); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'><span style='color: #ff0066;'>र</span><span style='color: #ff6f00;'>ू</span><span style='color: #ffjd00;'>प</span><span style='color: #4caf50;'>ा</span><span style='color: #2196f3;'>ं</span><span style='color: #9c27b0;'>त</span><span style='color: #ff5722;'>र</span><span style='color: #FFC0CB;'>क</span> <span style='color: #2196f3; font-size: 22px;'>~ Rupantarak</span><br><span style='font-size: 18px;'>By Vishal Sharma</span><br><span style='color: #ff0066; font-size: 22px;'>ॐ नमः पार्वती पतये, हर-हर महादेव:</span></h1></div>
     """
     display(HTML(logo_html))
 
@@ -79,9 +79,38 @@ progress.value += 1
 os.chdir('/content/Rupantarak')
 progress.value += 1
 
+# Note 1
+html_content_1 = '''
+<div style="display: flex; justify-content: center; margin-top: 10px; margin-bottom: 10px;">
+  <div style="width: 350px; margin: 10px 0;">
+    <details id="details1">
+      <summary id="summary1" style="border: none; border-radius: 8px; background: linear-gradient(to right, #FA8072, #F5F5DC); color: black; padding: 3px; text-align: center; cursor: pointer; list-style: none; font-weight: bold; margin-bottom: 10px;">
+        <span id="arrow1">➼</span> <b>Must-Knows</b>
+      </summary>
+      <div style="border-radius: 8px; background-color: #333333; padding: 7px; color: white;">
+        Hey there! Strap in for the GPU installation rodeo with our buddy रूपांतरक (Rupantarak). It's gonna be a wild 8-minute rollercoaster ride of tech wizardry. Grab some snacks, take a nap, or ponder life's mysteries while you wait. See you on the other side – hopefully with a shiny new GPU!
+    </details>
+  </div>
+</div>
+<script>
+  var details1 = document.getElementById('details1');
+  var arrow1 = document.getElementById('arrow1');
+  details1.addEventListener('toggle', function(event) {
+    if (details1.open) {
+      arrow1.textContent = '➷';
+    } else {
+      arrow1.textContent = '➼';
+    }
+  });
+</script>
+'''
+
+display(HTML(html_content_1))
 
 # Progress bar
 total_duration = 700
 layout = widgets.Layout(width='90%', margin='0 auto 0 auto')
 progress_bar = widgets.FloatProgress(value=0, min=0, max=100, layout=layout)
 display(progress_bar)
+
+%run /content/Rupantarak/Rupantarak_Pro/Rupantarak_I/रूपांतरक_GPU.py
